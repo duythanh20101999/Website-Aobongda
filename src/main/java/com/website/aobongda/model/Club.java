423879199182ebd588c911e601d6abcf08adfd04
ThanhTD <tranduythanhmutd1999@gmail.com>
2022-10-07 17:10:33 +0700

    Merge branch 'main' of https://github.com/duythanh20101999/Website-Aobongda.git into main

--- src/main/java/com/website/aobongda/model/Club.java
+++ src/main/java/com/website/aobongda/model/Club.java
@@ -1,5 +1,8 @@
 package com.website.aobongda.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -8,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -28,6 +32,8 @@ public class Club {
     
     @Column(nullable = false)
     private String name;
+    @OneToMany(mappedBy="club",cascade = CascadeType.REMOVE)
+    private List<Product> product;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="leauge_id")
